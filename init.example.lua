9420958184801bfbbe7a8640e041c4d5e05abc0d
Yuan Fu <casouri@gmail.com>
2018-10-07 14:29:57 -0400

    Update

--- init.example.lua
+++ init.example.lua
@@ -6,14 +6,22 @@ command_table={}
 -- Config
 --
 
--- Seal
+-- FnMate
 sh.useSpoon{
-   name = 'Seal',
-   config = function()
-      spoon.Seal:loadPlugins{'apps', 'calc', 'safari_bookmarks', 'screencapture', 'useractions', 'viscosity'}
-      spoon.Seal:start()
-   end
+   name = 'FnMate',
 }
+
+-- Seal
+-- sh.useSpoon{
+--    name = 'Seal',
+--    config = function()
+--       spoon.Seal:loadPlugins{'apps', 'calc', 'safari_bookmarks', 'screencapture'}
+--       spoon.Seal:start()
+--       spoon.Seal.chooser:bgDark(true)
+--       spoon.Seal.chooser:fgColor{hex='#DDDDDD', alpha=1}
+--       spoon.Seal.chooser:subTextColor{hex='#DDDDDD', alpha=1}
+--    end
+-- }
 
 -- Commander
 sh.useSpoon{
@@ -22,7 +30,6 @@ sh.useSpoon{
       spoon.Commander.chooser:bgDark(true)
       spoon.Commander.forceLayout = 'ABC'
    end
-
 }
 
 -- Emojis
@@ -48,6 +55,7 @@ sh.useSpoon{
 sh.useSpoon{
    name = 'RecursiveBinder',
    config = function()
+      spoon.RecursiveBinder.escapeKey = {{'control'}, 'g'}
       -- just a shortcut
       singleKey = spoon.RecursiveBinder.singleKey
       -- Spec of keymap:
@@ -60,9 +68,9 @@ sh.useSpoon{
       --    And the table have the same format of top table: keys to keys, value to table or function
       mymapWithName = {
          [{{}, 'space', 'Commander'}] = spoon.Commander.show,
-         [{{'shift'}, 'space', 'Seal'}] = function() spoon.Seal:show() end,
+         -- [{{'shift'}, 'space', 'Seal'}] = function() spoon.Seal:show() end,
          [singleKey('`', 'run command')] = runCommand,
-         [singleKey('f', 'find+')] = {
+         [singleKey('f', 'file+')] = {
             [singleKey('D', 'Desktop')] = function() openWithFinder('~/Desktop') end,
             [singleKey('p', 'Project')] = function() openWithFinder('~/p') end,
             [singleKey('d', 'Download')] = function() openWithFinder('~/Downloads') end,
@@ -70,10 +78,9 @@ sh.useSpoon{
             [singleKey('h', 'home')] = function() openWithFinder('~') end,
             [singleKey('f', 'hello')] = function() hs.alert.show('hello!') end},
          [singleKey('t', 'toggle+')] = {
-            [singleKey('v', 'file visible')] = function() hs.eventtap.keyStroke({'cmd', 'shift'}, '.') end
+            [singleKey('v', 'file visible')] = function() hs.eventtap.keyStroke({'cmd', 'shift'}, '.') end,
          },
          [singleKey('a', 'app+')] = {
-            [singleKey('a', 'open')] = spoon.OpenApp.show,
             [singleKey('e', 'Emacs')] = function() hs.application.launchOrFocus('Emacs') end,
             [singleKey('s', 'Safari')] = function() hs.application.launchOrFocus('Safari') end,
             [singleKey('f', 'Finder')] = function() hs.application.launchOrFocus('Finder') end,
@@ -90,12 +97,10 @@ sh.useSpoon{
             [singleKey('c', 'Console')] = function() hs.console.hswindow():focus() end,
             [singleKey('r', 'reload config')] = hs.reload,
          }
-
-
       }
 
       local keyNone = {}
-      hs.hotkey.bind({'shift'}, 'space', spoon.RecursiveBinder.recursiveBind(mymapWithName))
+      hs.hotkey.bind({'control', 'option'}, 'space', spoon.RecursiveBinder.recursiveBind(mymapWithName))
 
       spoon.RecursiveBinder.helperFormat.textFont = 'SF Mono'
       -- config ends here
@@ -116,3 +121,56 @@ sh.useSpoon{
    end
 }
 
+-- emacs-china
+
+local function Chinese()
+  hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM")
+end
+
+local function English()
+  hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+end
+
+local function set_app_input_method(app_name, set_input_method_function, event)
+  event = event or hs.window.filter.windowFocused
+
+  hs.window.filter.new(app_name)
+    :subscribe(event, function()
+                 set_input_method_function()
+              end)
+end
+
+set_app_input_method('Hammerspoon', English, hs.window.filter.windowCreated)
+set_app_input_method('Spotlight', English, hs.window.filter.windowCreated)
+set_app_input_method('Emacs', English)
+set_app_input_method('iTerm2', English)
+set_app_input_method('Safari', English)
+set_app_input_method('WeChat', Chinese)
+
+-- This method slows down keystokes
+
+-- cxBinding = hs.hotkey.bind({'control'}, 'x', function()
+--       print('C-x')
+--       cxBinding:disable()
+--       hs.eventtap.keyStroke({'control'}, 'x')
+--       switchInputMethod()
+--       -- to prevent infinite loop
+--       hs.timer.doAfter(1, function() cxBinding:enable() end)
+-- end)
+
+-- function switchInputMethod()
+--     if hs.execute('xkbswitch -g', true) ~= 2 then
+--        switchedInput = true
+--        hs.execute('xkbswtich -s 2', true)
+--     end
+-- end
+
+
+-- function switchInputMethodBack()
+--    if switchedInput then
+--       switchedInput = false
+--       hs.execute('xkbswitch -s 0', true)
+--    end
+-- end
+   
+-- hs.allowAppleScript(true)
